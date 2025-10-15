64d1e218126d006a91ce247b606f5e4e369f66b1
Braitiner <braitiner@gmail.com>
2025-10-15 16:22:00 -0300

    first commit

---
+++ src/main/java/br/com/livroDereceitas/LivroDereceitasApplication.java
@@ -0,0 +1,13 @@
+package br.com.livroDereceitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroDereceitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroDereceitasApplication.class, args);
+	}
+
+}
